398e9c2dcd6485ded56bd1ccab42fc33036f9b01
Racso <oscarhs0576@gmail.com>
2025-04-20 12:57:40 -0400

    Udated js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let count = 0
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-
+let countStr = 0
 function increment() {
     count += 1
     countEl.textContent = count
+   countStr= saveEl.textContent = "Previous entries: "+ count+ " - "
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContenttStr
-    countEl.textContent = 0
+    
+    countEl.textContent =0
     count = 0
 }
 
