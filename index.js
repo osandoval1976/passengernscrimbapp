6951c2e036b7bba44b88414a721d70aa38e1a62a
Racso <oscarhs0576@gmail.com>
2025-04-21 19:52:53 -0400

    update html & js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 let count = 0
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-let countStr = 0
+
 function increment() {
     count += 1
     countEl.textContent = count
-   countStr= saveEl.textContent = "Previous entries: "+ count+ " - "
+    
 }
 
 function save() {
-    
-    countEl.textContent =0
+   let countStr = count + "-"
+    saveEl.textContent +=countStr
+    countEl.textContent = 0 
     count = 0
 }
 
